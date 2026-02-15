1b11a7ad46d55c4316440c138790d1e94956c7fc
maltiez2 <maltiez2@gmail.com>
2026-02-15 11:28:54 +0300

    Add project files.

---
+++ source/ModSystems.cs
@@ -0,0 +1,15 @@
+﻿using Vintagestory.API.Common;
+
+namespace Backpacks;
+
+public sealed class BackpacksSystem : ModSystem
+{
+    public override void Start(ICoreAPI api)
+    {
+        api.RegisterCollectibleBehaviorClass("Backpacks:ShapeTexturesFromAttributes", typeof(ShapeTexturesFromAttributes));
+        api.RegisterCollectibleBehaviorClass("Backpacks:ShapeReplacement", typeof(ShapeReplacement));
+        api.RegisterCollectibleBehaviorClass("Backpacks:Sheath", typeof(SheathBehavior));
+        api.RegisterCollectibleBehaviorClass("Backpacks:Quiver", typeof(QuiverBehavior));
+        api.RegisterCollectibleBehaviorClass("Backpacks:VariantFromSlot", typeof(VariantFromSlotBehavior));
+    }
+}
